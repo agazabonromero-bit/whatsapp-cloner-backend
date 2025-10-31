c3556d3e614249d526cc795f5ae83b05de303491
agazabonromero-bit <agazabonromero@gmail.com>
2025-10-31 00:07:39 -0500

    Actualización CORS y Socket.IO para Vercel

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://whatsapp-clon-vfs7-git-main-alfredo-gazabons-projects.vercel.app"
+      "https://whatsapp-clon-6f67.vercel.app", 
+      "https://whatsapp-cloner-backend.onrender.com" 
     ],
     methods: ["GET", "POST"],
   },
